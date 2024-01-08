b1729fed00b9f1e5c1c1f49c7cf38b546f5e5db6
tim7107 <61227032+tim7107@users.noreply.github.com>
2024-01-08 15:27:16 +0800

    Add files via upload

--- CH4/CH4_main.c
+++ CH4/CH4_main.c
@@ -120,9 +120,3 @@ Overflow:
 	printf("[ERROR] Input Overflow!\n");
 	return 0;
 }
-
-/* Exercise 4-1
-*	Write the function strindex(s,t)
-*	which returns the position of the rightmost occurrence of t in s,
-*	or -1 if there is none.
-*/
